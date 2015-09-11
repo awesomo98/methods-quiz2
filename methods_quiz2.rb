e9c48189bff17bb242e13256c487796c82fe3d5f
Bilal Reza <rezasb@s.dcsdk12.org>
2015-09-11 09:52:46 -0600

    without_doubles complete

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,14 @@
-module MethodsQuiz
-	
+module MethodsQuiz2
+	def without_doubles(die1,die2)
+		if (die1 != die2)
+			return die1+die2
+		elsif (die1 == die2) && (die1 < 6)
+			return (die1 + 1) + die2			
+		end
+		if (die1 == die2) && (die1 == 6)
+			return (1 + die2)
+		end
+	end
 	
 
 end
